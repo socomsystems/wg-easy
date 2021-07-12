18d5c4e310207d8bfa023d7b510955908ba3055d
Troy Perkins <socomsystems@users.noreply.github.com>
2021-07-12 00:44:44 -0500

    Update config.js routing

--- src/config.js
+++ src/config.js
@@ -8,3 +8,5 @@ module.exports.WG_PORT = process.env.WG_PORT || 51820;
 module.exports.WG_DEFAULT_ADDRESS = process.env.WG_DEFAULT_ADDRESS || '10.8.0.x';
 module.exports.WG_DEFAULT_DNS = process.env.WG_DEFAULT_DNS || '1.1.1.1';
 module.exports.WG_ALLOWED_IPS = process.env.WG_ALLOWED_IPS || '0.0.0.0/0, ::/0';
+module.exports.WG_POSTUP = process.env.WG_POSTUP || 'PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE';
+module.exports.WG_POSTDN = process.env.WG_POSTDN || 'PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE';
